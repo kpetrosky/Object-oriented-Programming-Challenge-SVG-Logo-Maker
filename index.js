decc98beef9bcb7fe3a21e5f3f68239e2140ecd5
kpetrosky <kpetrosky36@gmail.com>
2023-04-07 12:59:05 -0700

    added lib with files for each requirement and started coding for svg, circle and testing

--- index.js
+++ index.js
@@ -1,24 +1,19 @@
 //npm i jest
 //npm i inquirer@8.2.4
+//npm init -y
+// GIVEN a command-line application that accepts user input
+// WHEN I am prompted for text
+// THEN I can enter up to three characters
+// WHEN I am prompted for the text color
+// THEN I can enter a color keyword (OR a hexadecimal number)
+// WHEN I am prompted for a shape
+// THEN I am presented with a list of shapes to choose from: circle, triangle, and square
+// WHEN I am prompted for the shape's color
+// THEN I can enter a color keyword (OR a hexadecimal number)
+// WHEN I have entered input for all the prompts
+// THEN an SVG file is created named `logo.svg`
+// AND the output text "Generated logo.svg" is printed in the command line
+// WHEN I open the `logo.svg` file in a browser
+// THEN I am shown a 300x200 pixel image that matches the criteria I entered
 
-class Circle {
-    constructor(x, y, radius, color, ctx) {
-      this.x = x; // x-coordinate of the center of the circle
-      this.y = y; // y-coordinate of the center of the circle
-      this.radius = radius; // radius of the circle
-      this.color = color; // fill color of the circle
-      this.ctx = ctx; // canvas 2d context
-    }
-  
-    draw() {
-      // Begin path for the circle
-      this.ctx.beginPath();
-      // Draw the circle
-      this.ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
-      // Set fill color for the circle
-      this.ctx.fillStyle = this.color;
-      // Fill the circle
-      this.ctx.fill();
-    }
-  }
-  
+
