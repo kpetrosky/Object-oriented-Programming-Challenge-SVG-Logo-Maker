0de9300d9dd13aa2c0ad8bc26aee4f48dcd9241e
kpetrosky <kpetrosky36@gmail.com>
2023-04-11 18:29:38 -0700

    trying to get svg test to work

--- index.js
+++ index.js
@@ -17,3 +17,90 @@
 // THEN I am shown a 300x200 pixel image that matches the criteria I entered
 
 
+function generateReadmeMarkdown(data) {
+    // TODO: Generate markdown content for README file based on user's responses
+    //generate markdown
+    //have to use markdown stringy to use data (readme for chall 9)
+    // using professional readme file
+    return `
+  # ${data.title}
+  
+  ## Description
+  
+  ${data.description}
+  
+  ## Table of Contents
+  
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [License](#license)
+  
+  ## Installation
+  
+  ${data.installation}
+  
+  ## Usage
+  
+  ${data.usage}
+  
+  ## Contributing
+  
+  ${data.contributing}
+  
+  ## Tests
+  
+  ${data.test}
+  
+  ## License
+  
+  This project is licensed under the ${data.license} license.
+  `;
+}
+
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+        if (err) throw err;
+        console.log(`The README was successfully created in ${fileName}.`);
+    });
+}
+
+function init() {
+    const inquirer = require('inquirer');
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'title',
+                message: 'What three letters will represent your app?',
+            },
+            {
+                type: 'input',
+                name: 'text color',
+                message: 'What color would you like your text to be?',
+            },
+            {
+                type: 'checkbox',
+                name: 'shape',
+                message: 'Choose a shape:',
+                choices: ['Circle', 'Square', 'Triangle']
+            },
+            {
+                type: 'input',
+                name: 'shape color',
+                message: 'What color would you like your shape to be?',
+            },
+         
+        ])
+        .then((response) => {
+            const markdown = generateReadmeMarkdown(response);
+            writeToFile('README.md', markdown);
+        });
+}
+
+// Function call to initialize app
+init();
+
+
+
